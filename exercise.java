1479c4a4f3df5199bf4de21e46895ae79267b204
Akashtunge <89512002+Akashtunge@users.noreply.github.com>
2021-08-25 14:47:03 +0530

    Add files via upload

---
+++ exercise.java
@@ -0,0 +1,11 @@
+public class Main {
+ 
+ public static void main(String[] args) {
+  System.out.println("Print the result of some specific operation:");
+  System.out.println("------------------------------");
+  System.out.println( "Result of 1st expression is:" +(-1+8*3));
+  System.out.println( "Result of 2nd expression is:"+(30+5) %4);
+  System.out.println("Result of 3rd expression is:"+(20 + 4*5 / 8));
+  System.out.println("Result of 4th expression is:"+(3+ 15 / 3 * 3 - 8 % 2));
+ }
+}
